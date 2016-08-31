832dd6c118ba01d030d94f87ca6c4c05722852ce
mikeyj777 <mikeyj777@gmail.com>
2016-08-31 09:58:03 -0400

    Initial Commit

---
+++ CrapsLearner/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  CrapsLearner
+//
+//  Created by macuser on 8/31/16.
+//  Copyright © 2016 ResponseApps. All rights reserved.
+//
+
+import Foundation
+
+class Player: NSObject {
+    
+    
+    
+}
